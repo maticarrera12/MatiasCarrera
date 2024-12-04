eb58f357e07cbbc4f645810099309df3ff1fe9ff
MatiCarrera <mcarreradev12@gmail.com>
2024-12-04 13:13:49 -0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="bg-gray-950 p-4">
       <div className="text-center mb-2">
         <p className="text-xs font-circular-web text-soft-white">
-          &copy; 2024 Matías Carrera. Todos los derechos reservados.
+          &copy; 2024 Matías Carrera.
         </p>
       </div>
       <div className="flex justify-center gap-4">
